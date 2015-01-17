4eee7c0b0ce1b350500289f5ea7222793a26f4d5
細矢 隆規 <bio2taka@hosoya-no-MacBook-Air.local>
2015-01-17 21:57:21 +0900

    雲のprefab作成

---
+++ Spring/Assets/Script/Cloud.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.Translate (speed, 0, 0);
+		if (this.transform.position.x > 5f) {
+			this.transform.position = new Vector3 (5f, this.transform.position.y, this.transform.position.z);
+			speed *= -1;
+		}
+		if (this.transform.position.x < -5f) {
+			this.transform.position = new Vector3 (-5f, this.transform.position.y, this.transform.position.z);
+			speed *= -1;
+		}
+	}
+}
