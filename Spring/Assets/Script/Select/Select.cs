99d19e4b77b2070f14d4ed7af751397421b8cbba
oeda.yuki <kamibiki.alf@gmail.com>
2015-01-17 14:11:14 +0900

    'タイトル画面、ステージ選択画面に仮画像設置'

--- Spring/Assets/Script/Select/Select.cs
+++ Spring/Assets/Script/Select/Select.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Title {
+public class Select {
 
 	// Use this for initialization
 	void Start ()
 	{
 	
 	}
-
+	
 	// Update is called once per frame
 	void Update ()
 	{
 		// tapされたかどうか
 		if (Input.touchCount > 0) {
 			// tapされたのでScene遷移
-			Application.LoadLevel ("SceneSelect");
+			Application.LoadLevel ("SceneResult");
 		}
 	}
 }
