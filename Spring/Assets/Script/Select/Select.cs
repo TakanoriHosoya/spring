10dc3759657a29f9c21a72ed578e862fbbc9551a
oeda.yuki <kamibiki.alf@gmail.com>
2015-01-17 14:42:18 +0900

    '取り合えずシーン切り替え'

--- Spring/Assets/Script/Select/Select.cs
+++ Spring/Assets/Script/Select/Select.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Select {
+public class Select  : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
@@ -13,7 +13,10 @@ public class Select {
 	void Update ()
 	{
 		// tapされたかどうか
-		if (Input.touchCount > 0) {
+		//if (Input.touchCount > 0) {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log(Input.mousePosition.x);
+			Debug.Log(Input.mousePosition.y);
 			// tapされたのでScene遷移
 			Application.LoadLevel ("SceneResult");
 		}
--- Spring/Assets/Script/Title/Title.cs
+++ Spring/Assets/Script/Title/Title.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Title {
+public class Title : MonoBehaviour {
 
 	// Use this for initialization
 	void Start ()
 	{
-	
+
 	}
 
 	// Update is called once per frame
 	void Update ()
 	{
 		// tapされたかどうか
-		if (Input.touchCount > 0) {
+		//if (Input.touchCount > 0) {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log(Input.mousePosition.x);
+			Debug.Log(Input.mousePosition.y);
 			// tapされたのでScene遷移
 			Application.LoadLevel ("SceneSelect");
 		}
