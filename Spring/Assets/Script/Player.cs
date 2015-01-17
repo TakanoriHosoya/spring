c90f4590d00c15db36387e21f1298e4f5f318d3a
str8ight <str8ight36@yahoo.co.jp>
2015-01-17 14:41:39 +0900

    メインページできている部分をpush

---
+++ Spring/Assets/Script/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnGUI() {
+		if (Event.current.type == EventType.MouseDown) {
+			Vector2 direction = new Vector2 (1, 1).normalized;
+			rigidbody2D.velocity = direction * speed;
+		}
+	}
+}
---
+++ Spring/Assets/Script/Wall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// 衝突時
+	void OnTriggerEnter2D (Collider2D col) {
+		if (col.gameObject.name == "Player") {
+		}
+	}
+}
