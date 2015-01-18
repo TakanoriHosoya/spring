f0045c6cb64d24826e0ef4f9a9e39201fdd4e164
細矢 隆規 <bio2taka@hosoya-no-MacBook-Air.local>
2015-01-18 11:05:28 +0900

    BGMマネージャーprefab 追加

---
+++ Spring/Assets/Script/BgmManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgmManager : MonoBehaviour {
+
+	public AudioClip mainBgm;
+	public AudioClip dashBgm;
+
+	// Use this for initialization
+	void Awake () {
+		//mainBgm = (AudioClip)Resources.Load ("Sound/stage");
+		//dashBgm = (AudioClip)Resources.Load ("Sound/speed_up");
+		//this.audio.clip = dashBgm;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.audio.isPlaying == false) {
+			this.audio.Play ();
+		}
+
+	}
+}
