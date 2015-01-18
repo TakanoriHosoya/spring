d75c26e0bc087eb58fe038e1ffdaa249a3035b79
細矢 隆規 <bio2taka@hosoya-no-MacBook-Air.local>
2015-01-18 09:34:22 +0900

    ステージをロードする処理

---
+++ Spring/Assets/Script/Gamemanager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gamemanager : MonoBehaviour {
+
+	public int stageNum = 1;
+
+	// Use this for initialization
+	void Awake () {
+		stageNum = PlayerPrefs.GetInt ("STAGE_NUM");
+		if (stageNum <= 0 && stageNum > 3 ) {
+			stageNum = 1;
+		}
+
+		GameObject stage = (GameObject)Instantiate(Resources.Load("Prefab/Stage_1", typeof(GameObject)));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
